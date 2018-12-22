78e6201b77757e7917d06081e6f5447d800f7cf7
codenewinsight <codeinsight@gmx.com>
2018-12-21 23:09:18 -0700

    add helper test file

--- migrations/2_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-//var Crowdsale = artifacts.require("./Crowdsale.sol");
-var Queue = artifacts.require("./Queue.sol");
-//var Token = artifacts.require("./Token.sol");
-
-module.exports = function(deployer) {
-	//deployer.deploy(Crowdsale);
-	deployer.deploy(Queue);
-	//deployer.deploy(Token);
-};
